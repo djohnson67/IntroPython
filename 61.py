d9cb5f759cd5ed30faf6644d8a7850d00d36fc0e
Dave Johnson <djohnson@Daves-MacBook-Pro-2.local>
2019-06-24 08:45:48 -0500

    more changes

--- 61.py
+++ 61.py
@@ -1,1 +1,8 @@
 print(61)
+
+days = ['Monday','Tuesday','Wednesday']
+fruits = ['banana','orange','peach']
+drinks = ['coffee','tea','beer']
+desserts = ['tiramisu','ice cream','pie','pudding']
+for day, fruit, drink, dessert in zip(days, fruits, drinks, desserts):
+  print(day,':drink', drink, '- eat', fruit, '- enjoy', dessert)
